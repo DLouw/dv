4d4a3b8f27e9c374704ab9444c246f8b132b1f99
dehanL <dehanlouw@icloud.com>
2018-08-10 14:31:39 +0200

    Desktop animation done

--- app/js/thescript.js
+++ app/js/thescript.js
@@ -44,6 +44,17 @@
 
 var controller = new ScrollMagic.Controller();
 
+var headerTimeline = new TimelineMax({paused:"true", delay:0.5});
+
+headerTimeline.from(".masthead .twn1", 1, {opacity:0, y:100})
+    .from(".masthead .twn2", 0.4, {opacity:0}, "+=0.6")
+    .from(".masthead .twn3", 0.4, {opacity:0, scale:2, color:"#dc2430", ease:Power2.easeIn}, "+= -0.4")
+    .from(".masthead .twn4", 0.4, {opacity:0, scale:2, color:"#dc2430",ease:Power2.easeIn}, "+=0.6")
+    .from(".masthead .twn5", 0.4, {opacity:0, scale:2, color:"#dc2430",ease:Power2.easeIn}, "+=0.6")
+    .from(".masthead .twn6", 0.2, {opacity:0}, "+=0.8")
+    .from("#mainNav", 0.2, {opacity:0}, "+= -0.2")
+    .to(".masthead .twn6", 0.4, {borderColor: "#dc2430"}, "+=0.6");
+
 $(document).ready(function(){
     
     var parts = ['info', '@', 'digivate', '.', 'co.za'];
@@ -55,6 +66,7 @@ $(document).ready(function(){
     });
     
     //Scroll animations
+    
     
     var whyusTween1 = TweenMax.from("#whyus .twn1", 1, {y: "300", opacity: 0, ease:Power2.easeOut});
     var whyusScene1 = new ScrollMagic.Scene({triggerElement:"#whyus"})
@@ -76,15 +88,28 @@ $(document).ready(function(){
 //            .setTween(featuresTween2)
 //            .addTo(controller);
     
-    var featuresTween2 = TweenMax.staggerFrom("#features .row1 p", 1, {opacity: 0, ease:Power2.easeOut}, 0.5);
+    var featuresTween2 = TweenMax.staggerFrom("#features .row1 .twn2", 1, {opacity: 0, ease:Power2.easeOut}, 0.5);
     var featuresScene2 = new ScrollMagic.Scene({triggerElement:"#features .row1"})
             .setTween(featuresTween2)
             .addTo(controller);
     
-    var featuresTween3 = TweenMax.staggerFrom("#features .row2 p", 1, {opacity: 0, ease:Power2.easeOut}, 0.5);
-    var featuresScene3 = new ScrollMagic.Scene({triggerElement:"#features .row2"})
-            .setTween(featuresTween3)
-            .addTo(controller);
+    
+    var bizoneTimeline = new TimelineMax();
+    
+    bizoneTimeline.staggerFrom("#bizone .twn1", 1, {opacity:0})
+        .from("#bizone .twn2", 0.4, {opacity:0}, "+=0.6")
+        .from("#bizone .twn3", 0.4, {opacity:0, y: 100}, "+=0.6")
+        .from("#bizone .twn4", 0.4, {opacity:0, x:100}, "+=0.5");
+    
+    var bizoneScene = new ScrollMagic.Scene({triggerElement:"#bizone .twn1"})
+        .setTween(bizoneTimeline)
+        .addTo(controller);
+    
+    var contactTween = new TweenMax.staggerFrom("#contact .icon", 0.9, {opacity: 0, x: 100}, 0.3);
+    
+    var contactScene = new ScrollMagic.Scene({triggerElement:"#contact"})
+        .setTween(contactTween)
+        .addTo(controller);
     
 //    var featuresTween3 = TweenMax.staggerFrom("#features p", 1, {opacity: 0, ease:Power2.easeOut}, 0.5);
 //    var featuresScene3 = new ScrollMagic.Scene({triggerElement:"#features", triggerHook: "-0.6"})
@@ -100,6 +125,7 @@ function splash(param) {
        $("#splash").animate({opacity: 0}, 500, function(){
            $(this).hide();
            $("body").removeClass("no-scroll");
+           headerTimeline.play();
        }); 
     }, time);
 }
